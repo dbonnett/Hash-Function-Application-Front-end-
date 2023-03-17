593c7f89a9f42bee0e75990d1f79af1f0f945ff6
Danny B <dbonnett1147@gmail.com>
2023-03-17 16:09:57 -0400

    switched project to vue

---
+++ hash-function/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <FormulaInput></FormulaInput>
+</template>
+
+<script>
+import FormulaInput from './components/FormulaInput.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FormulaInput
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hash-function/src/components/FormulaInput.vue
@@ -0,0 +1,21 @@
+<!-- eslint-disable vue/comment-directive -->
+<template>
+  <div>
+    <p>Hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FormulaInput',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+    p {
+        size: 12;
+    }
+</style>
