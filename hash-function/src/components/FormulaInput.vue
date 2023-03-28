e52a8495aa9d3c06cf7283b0b16191e89d5bac8b
Danny B <dbonnett1147@gmail.com>
2023-03-28 16:00:47 -0400

    binding data in vue

--- hash-function/src/components/FormulaInput.vue
+++ hash-function/src/components/FormulaInput.vue
@@ -2,20 +2,33 @@
 <template>
   <div>
     <p>Hello</p>
+    <p>Size of Table: </p>
+    <input type="number" v-model="sizeOfTable"/>
+    <p>Coefficient: </p>
+    <input type="number" value="13"/>
+    <p>Seed: </p>
+    <input type="number" value="0"/>
+
+    <p>Formula: </p>
+    <p>Enter a word</p>
+    <input type="text">
   </div>
 </template>
 
 <script>
 export default {
     name: 'FormulaInput',
-    props: {
-        msg: String
+    data() {
+      return {
+        sizeOfTable: 50,
+        coefficient: 13
+      }
     }
 }
 </script>
 
 <style>
     p {
-        size: 12;
+        size: 16;
     }
 </style>
